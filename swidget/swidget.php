a7830dd6af2b44afa3c0e3ab469b31f0dc74da32
Drew McDermott <code@drewmcdermott.net>
2017-08-10 10:14:57 -0400

    Bump version

--- swidget/swidget.php
+++ swidget/swidget.php
@@ -3,7 +3,7 @@
 Plugin Name: Swidget
 Plugin URI: https://github.com/CMP-Studio/swidget-wordpress
 Description: Siriusware Widget
-Version: 0.9
+Version: 0.10.000
 Author: Carnegie Museums of Pittsburgh
 Author URI: http://www.carnegiemuseums.org
 License: GPLv2 or later
